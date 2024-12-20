4753373a14f03f8956be54a0cafa5f2ce4e4fdbe
underground0930 <go.nishiduka.1985@gmail.com>
2024-12-20 17:06:44 +0900

    update

--- tanstack-table-practice/src/App.tsx
+++ tanstack-table-practice/src/App.tsx
@@ -5,6 +5,9 @@ function App() {
 
   return (
     <>
+    <div className="h-[60px] bg-red-500 fixed top-0 left-0 w-full z-10">
+
+    </div>
     <div className='pt-[300px]'>
       {/* <Sample /> */}
       <Test />
